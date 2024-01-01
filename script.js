691e554e0415a1713d4ab8608919d8de63fc91cc
caleb1711 <>
2024-01-01 10:46:13 +0000

    set the structure of the Game

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ var opponentScore = 0;
 
 var choices = ["rock", "paper", "scissors"];
 
+// function populate game choices
 window.onload = function () {
     for (let i = 0; i < 3; i++) {
         // <img id="rock" src="rock.png">
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
-        // choice.addEventListener("click", selectChoice);
+        choice.addEventListener("click", selectChoice);
         document.getElementById("choices").append(choice);
     }
 };
