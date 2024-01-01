8ab8dda1bb7ab0322ba6b9e041e2f0f7c63ee08b
caleb1711 <>
2024-01-01 10:37:22 +0000

    set function to populate the games choices, when the page loads

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ var opponent;
 var opponentScore = 0;
 
 var choices = ["rock", "paper", "scissors"];
+
+window.onload = function () {
+    for (let i = 0; i < 3; i++) {
+        // <img id="rock" src="rock.png">
+        let choice = document.createElement("img");
+        choice.id = choices[i];
+        choice.src = choices[i] + ".png";
+        // choice.addEventListener("click", selectChoice);
+        document.getElementById("choices").append(choice);
+    }
+};
