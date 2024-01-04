1ddd84679e3c102c13ce5b508c7399880a025ffc
caleb1711 <>
2024-01-04 21:56:04 +0000

    delete commented out code lines

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var choices = ["rock", "paper", "scissors"];
 // function populate game choices
 window.onload = function () {
     for (let i = 0; i < 3; i++) {
-        // <img id="rock" src="rock.png">
         let choice = document.createElement("img");
         choice.id = choices[i];
         choice.src = choices[i] + ".png";
