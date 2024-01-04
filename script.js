e927cd9f6117c7b813822e1d119d582d6fa0cd9a
caleb1711 <>
2024-01-04 21:32:10 +0000

    add style to the opponent score and your score

--- script.js
+++ script.js
@@ -17,49 +17,53 @@ window.onload = function () {
     }
 };
 
+function startNewGame(){
+    document.getElementById("reset").click = yourScore = 0;
+
+}
 
 function selectChoice() {
     you = this.id;
     document.getElementById("your-choice").src = you + ".png";
 
-    //random for oppponent
-    opponent = choices[Math.floor(Math.random() * 3)]; //0- .999999 * 3 = 0-2.99999
+    //random choices for oppponent
+    opponent = choices[Math.floor(Math.random() * 3)];
     document.getElementById("opponent-choice").src = opponent + ".png";
 
     //check for winner
     if (you == opponent) {
         yourScore += 0;
         opponentScore += 0;
-        
+
     }
 
     else {
         if (you == "rock") {
             if (opponent == "scissors") {
                 yourScore += 1;
+            }
+            else if (opponent == "paper") {
+                opponentScore += 1;
+            }
         }
-        else if (opponent == "paper") {
-            opponentScore += 1;
-        }
-    }
         else if (you == "scissors") {
             if (opponent == "paper") {
                 yourScore += 1;
+            }
+            else if (opponent == "rock") {
+                opponentScore += 1;
+            }
         }
-        else if (opponent == "rock") {
-            opponentScore += 1;
-        }
-    }
         else if (you == "paper") {
             if (opponent == "rock") {
                 yourScore += 1;
-        }
-        else if (opponent == "scissors") {
-            opponentScore += 1;
+            }
+            else if (opponent == "scissors") {
+                opponentScore += 1;
+            }
         }
     }
-}
 
-document.getElementById("your-score").innerText = yourScore;
-document.getElementById("opponent-score").innerText = opponentScore;
+    document.getElementById("your-score").innerText = yourScore;
+    document.getElementById("opponent-score").innerText = opponentScore;
 }
